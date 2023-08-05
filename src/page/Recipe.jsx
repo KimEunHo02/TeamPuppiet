e656fa991b029ece0e5712854dbe49d6f855ee1a
seoyumin1018 <yumin1018@kakao.com>
2023-08-05 23:34:11 +0900

    0805 23:33
    간식 레시피 페이지 화면 설계 (미완)

--- src/page/Recipe.jsx
+++ src/page/Recipe.jsx
@@ -1,74 +1,99 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import picSrc from '../img/logo_name(x).png'
-
+import React, { useState } from 'react';
+import Logo from './Logo';
+import Button from 'react-bootstrap/Button';
 
 const Recipe = () => {
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-  const box1 = {
-    margin: '0 auto',
-    padding: '20px',
-    width: '800px',
-    height: '300px',
-    backgroundColor: 'white',
-    marginBottom: '100px',
-    borderRadius: '20px',
-    padding: '20px',
-    textAlign: 'center',
-    fontSize: '19px',
-    fontWeight: 'bold',
-  }
+  const ingredients = [
+    '고구마', '감자', '당근', '호박', '닭고기', '돼지고기', '소고기', '연어',
+    '사과', '바나나', '꿀', '귀리', '우유', '요거트', '치즈', '달걀'
+  ];
+
+  // 재료 선택/해제 핸들러
+  const handleIngredientClick = (ingredient) => {
+    if (selectedIngredients.includes(ingredient)) {
+      setSelectedIngredients(selectedIngredients.filter(item => item !== ingredient));
+    } else {
+      setSelectedIngredients([...selectedIngredients, ingredient]);
+    }
+  };
+
+  // 재료 버튼 스타일
+  const ingredientButtonStyle = {
+    backgroundColor: '#F0F0F0',
+    color: 'gray',
+    border: 'none',
+    fontSize: '13px',
+    width: '90px',
+    height: '40px',
+    marginBottom: '10px',
+  };
+
+  // 재료 버튼 두 줄로 나눠 표시하기 위한 배열
+  const ingredientsRows = [];
+  const firstRowIngredients = ingredients.slice(0, 8);
+  const secondRowIngredients = ingredients.slice(8);
+
+  // 첫 번째 줄에 재료 버튼 추가
+  ingredientsRows.push(
+    <div key={1} style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+      {firstRowIngredients.map((ingredient, index) => (
+        <Button
+          key={index}
+          variant={selectedIngredients.includes(ingredient) ? 'secondary' : 'primary'}
+          size="lg"
+          style={{ ...ingredientButtonStyle, marginRight: '10px', backgroundColor: selectedIngredients.includes(ingredient) ? '#FFC9C9' : '#F0F0F0' }}
+          onClick={() => handleIngredientClick(ingredient)}
+        >
+          {ingredient}
+        </Button>
+      ))}
+    </div>
+  );
+
+  // 두 번째 줄에 재료 버튼 추가
+  ingredientsRows.push(
+    <div key={2} style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+      <div style={{ flex: '1' }}></div>
+      {secondRowIngredients.map((ingredient, index) => (
+        <Button
+          key={index}
+          variant={selectedIngredients.includes(ingredient) ? 'secondary' : 'primary'}
+          size="lg"
+          style={{ ...ingredientButtonStyle, marginRight: '10px', backgroundColor: selectedIngredients.includes(ingredient) ? '#FFC9C9' : '#F0F0F0' }}
+          onClick={() => handleIngredientClick(ingredient)}
+        >
+          {ingredient}
+        </Button>
+      ))}
+      <div style={{ flex: '1' }}></div>
+    </div>
+  );
 
   return (
-    <div style={{backgroundColor : '#F0F0F0'}}>
-      {/* ----------- 상단바 ------------ */}
+    <div style={{ backgroundColor: '#F0F0F0' }}>
+      <Logo />
 
-      <header style={{ backgroundColor: '#F0F0F0' }} className="header" >
+      <div style={{ margin: '20px auto', width: '1000px', backgroundColor: 'white', marginBottom: '100px', borderRadius: '20px', padding: '20px', textAlign: 'center', fontSize: '19px', fontWeight: 'bold' }}>
+        <p style={{ color: 'gray', fontSize: '15px', margin: '10px', marginBottom: '25px' }}>재료를 선택해 주세요.</p>
+        
+        {/* 두 줄로 배치 */}
+        {ingredientsRows}
 
-        <div style={{
-          display: 'flex', alignItems: 'center',
-          justifyContent: 'space-between', padding: '10px'
-        }}>
-          {/* 텍스트 */}
-          <div>
-            <h1 style={{ fontSize: '30px' }} className='puppiettxt'>PUPPIET</h1>
-          </div>
-
-          {/* 이미지 */}
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Link to='/'>
-              <img className='logo' src={picSrc} width='150px' alt="Logo"></img>
-            </Link>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          {/* 개견정보, 마이페이지, 로그아웃 (로고 + 링크) */}
-          <Link to="/main2" style={{ marginRight: '30px' }} className='maintext'>반려견 정보</Link>
-          <Link to="/mypage" style={{ marginRight: '30px' }} className='maintext'>마이페이지</Link>
-          <Link to="/main2" style={{ marginRight: '30px' }} className='maintext'>로그아웃</Link>
-
-          </div>
+        {/* 초기화와 검색하기 버튼 */}
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+          <Button variant="light" style={{ ...ingredientButtonStyle, marginRight: '20px', backgroundColor: '#F0F0F0', color: 'gray', width: '100px' }}>
+            초기화
+          </Button>
+          <Button variant="primary" style={{ ...ingredientButtonStyle, marginRight: '100px', backgroundColor: '#F0F0F0', color: 'gray', width: '100px' }}>
+            검색하기
+          </Button>
         </div>
-      </header>
-
-      {/* 타이틀 */}
-      <div className='bar-container'>
-        <Link to="/information" className='bartext'>정보 보기</Link>
-        <Link to="/feed" className='bartext'>사료 추천</Link>
-        <Link to="/recipe" className='bartext'>간식 레시피 추천</Link>
-        <Link to="/calendar" className='bartext'>캘린더</Link>
       </div>
-
-      {/* ------------------------------------- */}
-      {/* 재료 선택 */}
-      <div style={{...box1, marginTop : '20px'}}>
-        <p>재료를 선택해 주세요.</p>
-      </div>
-
-      {/* n개의 간식 레시피 추천 */}
-
     </div>
+  );
+};
 
-  )
-}
-
-export default Recipe
+export default Recipe;
