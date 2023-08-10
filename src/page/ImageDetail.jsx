6bc4e996369892a1163f84c7e7309203b081c507
seoyumin1018 <yumin1018@kakao.com>
2023-08-10 11:11:06 +0900

    0810 11:10
    사료추천 간식레시피 추천 상세페이지 공간 화면 설계

--- src/page/ImageDetail.jsx
+++ src/page/ImageDetail.jsx
@@ -10,20 +10,39 @@ const ImageDetail = () => {
       <Logo/>
   
       {/* 간식 레시피 담을 div */}
-      <div style={{ display: 'flex', justifyContent: 'center', margin: '20px auto', width: '1200px', backgroundColor: 'white', marginBottom: '50px', borderRadius: '20px', padding: '20px', fontSize: '19px', fontWeight: 'bold', position: 'relative' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', margin: '30px auto', width: '1200px', backgroundColor: 'white', marginBottom: '50px', borderRadius: '20px', padding: '20px', fontSize: '19px', fontWeight: 'bold', position: 'relative' }}>
         
         {/* 왼쪽 이미지 넣을 div */}
-        <div style={{ flex: 0.5, backgroundColor: 'lightyellow', padding: '20px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
-          이미지
-          {/* 이미지 내용 */}
+        <div style={{ flex: 1, backgroundColor: 'white', padding: '20px', paddingTop: '30px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
+          
+          {/* 이미지 */}
+          <img src={process.env.PUBLIC_URL + '/recipeimg.png'} alt="사료 이미지" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+          {/* 레시피 이름 */}
+          <div style={{marginTop: '40px'}}>
+            <h3 style={{backgroundColor: '#FFC9C9', width: '160px', margin: 'auto', borderRadius: '20px', marginBottom: '10px'}}>레시피 이름</h3><br/>
+          </div>
         </div>
   
         {/* 오른쪽 텍스트 넣을 div */}
-        <div style={{ flex: 1, backgroundColor: 'lightblue', padding: '20px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
+        <div style={{ flex: 1, backgroundColor: 'white', padding: '20px', paddingTop: '30px',fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
           {/* 텍스트 내용 */}
-          <h3>레시피 이름</h3><br/>
-          <h5>레시피 재료</h5><br/>
-          <h5>레시피 조리 순서</h5>
+
+          {/* 레시피 재료 */}
+          <div style={{marginBottom: '50px'}}>
+            <h3 style={{backgroundColor: '#FFC9C9', width: '160px', margin: 'auto', borderRadius: '20px', marginBottom: '10px'}}>레시피 재료</h3><br/>
+            <p>레시피 재료들 들어가야 합니다 요기에~</p>
+          </div>
+
+          {/* 레시피 조리 순서 */}
+          <div>
+            {/* p 태그로 감싸던지 br로 나누던지 해야함 */}
+            <h3 style={{backgroundColor: '#FFC9C9', width: '200px', margin: 'auto', borderRadius: '20px', marginBottom: '20px'}}>레시피 조리 순서</h3>
+            <p>1. 어쩌고 저쩌고 한다</p>
+            <p>2. 저쩌고 저쩌고 저쩌고 한다</p>
+            <p>3. 어쩌고 저쩌고 어쩌고 저쩌고 한다</p>
+            <p>4. 반려견에게 급여하세요!</p>
+          </div>
         </div>
         
       </div>
--- src/page/ImageDetailFeed.jsx
+++ src/page/ImageDetailFeed.jsx
@@ -7,29 +7,48 @@ const ImageDetailFeed = () => {
 
   return (
     <div>
-      <Logo/>
+      <Logo />
 
       {/* 사료 담을 div */}
-      <div style={{ display: 'flex', justifyContent: 'center', margin: '20px auto', width: '1200px', backgroundColor: 'white', marginBottom: '50px', borderRadius: '20px', padding: '20px', fontSize: '19px', fontWeight: 'bold', position: 'relative' }}>
-        
+      <div style={{ display: 'flex', justifyContent: 'center', margin: '30px auto', width: '1200px', backgroundColor: 'white', marginBottom: '50px', borderRadius: '20px', padding: '20px', fontSize: '19px', fontWeight: 'bold', position: 'relative' }}>
+
         {/* 왼쪽 이미지 넣을 div */}
-        <div style={{ flex: 0.5, backgroundColor: 'lightyellow', padding: '20px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
-          이미지
-          {/* 이미지 내용 */}
+        <div style={{ flex: 0.7, width: '400px', backgroundColor: 'white', padding: '20px', paddingTop: '30px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
+          {/* 이미지 */}
+          <img src={process.env.PUBLIC_URL + '/saryoimg.jpg'} alt="사료 이미지" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+          {/* 사료 이름 */}
+          <div style={{ marginTop: '10px' }}>
+            <h3 style={{ backgroundColor: '#FFC9C9', width: '140px', margin: 'auto', borderRadius: '20px', marginBottom: '10px' }}>사료 이름</h3><br />
+          </div>
         </div>
-  
+
         {/* 오른쪽 텍스트 넣을 div */}
-        <div style={{ flex: 1, backgroundColor: 'lightblue', padding: '20px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
+        <div style={{ flex: 1, backgroundColor: 'white', padding: '20px', paddingTop: '30px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
+
           {/* 텍스트 내용 */}
-          <h3>사료 이름</h3><br/>
-          <h5>사료 성분</h5><br/>
-          <h5>사료 설명</h5>
+
+          {/* 사료 설명 */}
+          <div style={{marginBottom: '50px'}}>
+            <h3 style={{ backgroundColor: '#FFC9C9', width: '140px', margin: 'auto', borderRadius: '20px', marginBottom: '10px' }}>사료 설명</h3><br />
+            {/* p 태그로 감싸던지 br로 나누던지 해야함 */}
+            <p>1. 맛있는 사료!</p>
+            <p>2. 최고의 사료!</p>
+            <p>3. 최고의 사료!</p>
+            <p>4. 최고의 사료!</p>
+          </div>
+
+          {/* 사료 성분 */}
+          <div>
+            <h3 style={{ backgroundColor: '#FFC9C9', width: '140px', margin: 'auto', borderRadius: '20px', marginTop: '10px' }}>사료 성분</h3><br />
+            <p>사료는 맛있게 뭐랑 뭐랑 뭐랑 뭐랑 들어갔다</p>
+          </div>
         </div>
-        
+
       </div>
 
       {/* 나중에 DB 받으면 feedId 값으로 받아오기 */}
-      <h6>Feed 상세 페이지 - {feedId}</h6> 
+      <h6>Feed 상세 페이지 - {feedId}</h6>
     </div>
   );
 };
