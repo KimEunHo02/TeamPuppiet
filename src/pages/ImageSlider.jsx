30de4aecefe0f2adf4ebb00a4d7b94b1474c1067
KimEunHo02 <140569096+KimEunHo02@users.noreply.github.com>
2023-08-11 17:20:49 +0900

    08-11
    메인페이지 사료(이미지,이름)/ 간식(이미지,이름) 랜덤으로보여주기

--- src/pages/ImageSlider.jsx
+++ src/pages/ImageSlider.jsx
@@ -1,30 +1,30 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/ImageSlider.css'; // 스타일링을 위한 CSS 파일
 import axios from 'axios';
 
-// Mainpage1에서 이미지 슬라이더에 쓰이는 컴포넌트 공간입니다
-// 사료 이미지 슬라이더
-
-const ImageSlider = ({ images }) => {
+const ImageSlider = () => {
   const [startIndex, setStartIndex] = useState(0);
-  const [data, setData] = useState([]); // 사료명 데이터를 저장할 상태
+  const [data, setData] = useState([]);
+  const imageCount = 181; // 사료 이미지의 총 개수
 
   useEffect(() => {
-    axios.get('건식사료-성분.json') // JSON 파일의 경로를 적절히 수정해주세요.
+    axios.get('dryfeed.json') // 데이터 JSON 파일 경로를 수정해주세요
       .then(response => {
         setData(response.data);
-
-        const randomIndex = Math.floor(Math.random() * (images.length - 3)); // 랜덤한 시작 인덱스 계산
-        setStartIndex(randomIndex);
+        setRandomStartIndex(imageCount);
       })
       .catch(error => {
         console.error('데이터를 불러오는데 에러 발생:', error);
       });
   }, []);
 
+  const setRandomStartIndex = (count) => {
+    const randomIndex = Math.floor(Math.random() * (count - 3));
+    setStartIndex(randomIndex);
+  };
 
   const handleNextClick = () => {
-    if (startIndex < images.length - 1) {
+    if (startIndex < imageCount - 3) {
       setStartIndex(startIndex + 1);
     }
   };
@@ -35,24 +35,25 @@ const ImageSlider = ({ images }) => {
     }
   };
 
-  const visibleImages = images.slice(startIndex, startIndex + 3);
-
   return (
     <div className="image-slider">
       <button className="prev-button" onClick={handlePrevClick} disabled={startIndex === 0}>
         &lt;
       </button>
       <div className="image-container">
-        {visibleImages.map((image, index) => (
-          <div key={index} className="foodbox">
-            <img src={`건식사진/건식${startIndex + index + 1}.jpg`} alt={`Image ${startIndex + index + 1}`} />
-
-            {/* 사료 제품명 들어갈 텍스트 공간 -> 데이터 넣을 시 수정 필요 */}
-            <a className="foodtext">{data[startIndex + index]?.Column2}</a>
-          </div>
-        ))}
+        {[0, 1, 2].map((index) => {
+          const imageIndex = (startIndex + index) % imageCount + 1;
+          const imageName = `건식사진/건식${imageIndex}.jpg`;
+          const imageData = data[imageIndex - 1]?.Column2; // 이미지 인덱스에 해당하는 사료명 데이터 가져오기
+          return (
+            <div key={index} className="foodbox">
+              <img src={imageName} alt={`Image ${imageIndex}`} />
+              <a className="foodtext">{imageData}</a>
+            </div>
+          );
+        })}
       </div>
-      <button className="next-button" onClick={handleNextClick} disabled={startIndex === images.length - 3}>
+      <button className="next-button" onClick={handleNextClick} disabled={startIndex === imageCount - 3}>
         &gt;
       </button>
     </div>
