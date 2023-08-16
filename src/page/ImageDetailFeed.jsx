b5d61e0a50ea076ca7f3fbb22f6142ba99bf41b9
seoyumin1018 <yumin1018@kakao.com>
2023-08-16 11:05:26 +0900

    0816 11:05
    사료 상세 페이지 수정

--- src/page/ImageDetailFeed.jsx
+++ src/page/ImageDetailFeed.jsx
@@ -17,7 +17,7 @@ const getImageFilePath = (feedId) => {
     return {
       imagePath: `${process.env.PUBLIC_URL}/습식사진/습식${wetFeedId}.jpg`,
       nameData: `${process.env.PUBLIC_URL}/습식사료-성분.json`,
-      descriptionData: `${process.env.PUBLIC_URL}/습식사료특징.json`, 
+      descriptionData: `${process.env.PUBLIC_URL}/습식사료특징.json`,
     };
   }
 };
@@ -34,16 +34,16 @@ const ImageDetailFeed = () => {
   const [feedDescriptions, setFeedDescriptions] = useState([]);
 
 
-  
+
   const fetchFeedNameData = (dataUrl, targetId) => {
     const imageInfo = getImageFilePath(targetId); // imageInfo를 가져오기
-  
+
     fetch(dataUrl)
       .then((response) => response.json())
       .then((data) => {
         const info = data.find((item) => item.Column1 === targetId);
         setSelectedFeedName(info ? info.사료명 : '사료 정보 없음');
-  
+
         if (targetId > 181) {
           fetchFeedDescriptionData(imageInfo.descriptionData, targetId - 181);
         } else if (targetId <= 181 && info) {
@@ -74,24 +74,24 @@ const ImageDetailFeed = () => {
 
   useEffect(() => {
     console.log("사료 ID에 대한 데이터 가져오는 중:", selectedFeed.id);
-    
+
     // getImageFilePath 함수를 호출하여 imageInfo를 가져옴
     const imageInfo = getImageFilePath(selectedFeed.id);
-    
-  
+
+
     // if 문으로 imageInfo가 정의되어 있는지 체크하고 데이터를 가져오는 함수 호출
     if (imageInfo.nameData) {
       fetchFeedNameData(imageInfo.nameData, selectedFeed.id);
       if (selectedFeed.id <= 181) {
         const dryDataUrl = `${process.env.PUBLIC_URL}/건식사료-성분.json`;
         fetchFeedData(dryDataUrl, selectedFeed.id);
-  
+
         // fetchFeedDescriptionData를 호출하도록 수정
         fetchFeedDescriptionData(imageInfo.descriptionData, selectedFeed.id);
       } else {
         const wetDataUrl = `${process.env.PUBLIC_URL}/습식사료-성분.json`;
         fetchFeedData(wetDataUrl, selectedFeed.id - 181);
-  
+
         // fetchFeedDescriptionData를 호출하도록 수정
         fetchFeedDescriptionData(imageInfo.descriptionData, selectedFeed.id - 181);
       }
@@ -147,7 +147,7 @@ const ImageDetailFeed = () => {
       <Logo />
 
       {/* 사료 담을 div */}
-      <div style={{ display: 'flex', justifyContent: 'center', margin: '30px auto', width: '1200px', backgroundColor: 'white', marginBottom: '50px', borderRadius: '20px', padding: '20px', fontSize: '19px', fontWeight: 'bold', position: 'relative' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', margin: '30px auto', width: '1200px', backgroundColor: 'white', marginBottom: '50px', borderRadius: '20px', padding: '20px', fontSize: '19px', fontWeight: 'bold', position: 'relative', border: 'solid #FFC9C9 7px' }}>
 
         {/* 왼쪽 이미지 넣을 div */}
         <div style={{ flex: 1, width: '400px', backgroundColor: 'white', padding: '20px', paddingTop: '30px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
@@ -156,44 +156,46 @@ const ImageDetailFeed = () => {
             src={getImageFilePath(selectedFeed.id).imagePath}
             alt="사료 이미지"
             style={{ maxWidth: '100%', maxHeight: '100%' }}
-/>
+          />
 
           {/* 사료 이름 */}
           <div style={{ marginTop: '40px' }}>
-            <h3 style={{ backgroundColor: '#FFC9C9', width: '400px', margin: 'auto', borderRadius: '20px', marginBottom: '10px' }}>{selectedFeedName}</h3><br />
+            <h3 style={{ width: '550px', margin: 'auto', borderRadius: '20px', marginBottom: '10px', fontSize: '25px' }}>{selectedFeedName}</h3><br />
           </div>
         </div>
 
+        {/* 세로 선 */}
+        <div style={{ width: '2px', backgroundColor: '#FFC9C9' }}></div>
+
+
         {/* 오른쪽 텍스트 넣을 div */}
-        <div style={{ flex: 1, backgroundColor: 'white', padding: '20px', paddingTop: '30px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
+        <div style={{ flex: 1, backgroundColor: 'white', padding: '20px', paddingTop: '80px', fontSize: '19px', fontWeight: 'bold', textAlign: 'center' }}>
 
           {/* 텍스트 내용 */}
 
           {/* 사료 설명 */}
-          <div style={{ marginBottom: '50px' }}>
-            <h3 style={{ backgroundColor: '#FFC9C9', width: '200px', margin: 'auto', borderRadius: '20px', marginBottom: '10px' }}>🍖 사료 설명 🍖</h3>
+          <div style={{ marginBottom: '50px', backgroundColor: 'white', marginLeft: '15px' }}>
+            {/* <strong style={{ fontSize: '30px',  width: '250px', borderRadius: '20px', marginBottom: '10px', textAlign: 'center' }}>🍖 사료 설명 🍖</strong> */}
             {/* selectedFeedDescription이 정의되어 있을 때만 split 메서드 호출 */}
             {selectedFeedDescription && selectedFeedDescription.split('\n').map((item, index) => (
               <p key={index}>{item}</p>
             ))}
           </div>
+
           {/* 사료 성분 */}
-          <div>
-            <h3 style={{ backgroundColor: '#FFC9C9', width: '200px', margin: 'auto', borderRadius: '20px', marginTop: '10px' }}>사료 성분</h3><br />
-            <p>조단백질(%): {selectedFeedNutrition["조단백질(%)"]}</p>
-            <p>조지방(%): {selectedFeedNutrition["조지방(%)"]}</p>
-            <p>조섬유(%): {selectedFeedNutrition["조섬유(%)"]}</p>
-            <p>조회분(%): {selectedFeedNutrition["조회분(%)"]}</p>
-            <p>인(%): {selectedFeedNutrition["인(%)"]}</p>
-            <p>칼슘(%): {selectedFeedNutrition["칼슘(%)"]}</p>
-            <p>수분(%): {selectedFeedNutrition["수분(%)"]}</p>
+          <div style={{ marginLeft: '15px' }}>
+            <div>
+              <strong style={{ fontSize: '30px', width: '200px', borderRadius: '20px', marginTop: '10px' }}>성분</strong>
+              <p style={{ marginTop: '12px'}}>조지방(%): {selectedFeedNutrition["조지방(%)"]}</p>
+              <p>조섬유(%): {selectedFeedNutrition["조섬유(%)"]}</p>
+              <p>조회분(%): {selectedFeedNutrition["조회분(%)"]}</p>
+              <p>인(%): {selectedFeedNutrition["인(%)"]}</p>
+              <p>칼슘(%): {selectedFeedNutrition["칼슘(%)"]}</p>
+              <p>수분(%): {selectedFeedNutrition["수분(%)"]}</p>
+            </div>
           </div>
         </div>
-
       </div>
-
-      {/* 나중에 DB 받으면 feedId 값으로 받아오기 */}
-      <h6>Feed 상세 페이지 - {feedId}</h6>
     </div>
   );
 };
